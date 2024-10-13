1ba424696ed3f16f8374679ee238faef07b64a21
tuyishimekyrie <tuyishimehope01@gmail.com>
2024-10-13 20:03:15 +0200

    initial commit

---
+++ src/main/java/rw/ac/auca/kuzahealth/KuzahealthServerApplication.java
@@ -0,0 +1,13 @@
+package rw.ac.auca.kuzahealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuzahealthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuzahealthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rw/ac/auca/kuzahealth/KuzahealthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.ac.auca.kuzahealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuzahealthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
